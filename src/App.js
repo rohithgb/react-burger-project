4c3b06fd69be09a806e6d5f75a68a555e4dc6a00
rohithgb <rohgb@outlook.com>
2019-01-05 23:59:14 +0530

    Project structure change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// import Aux from './hoc/Aux';
-import Layout from './components/Layout/Layout';
+
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
